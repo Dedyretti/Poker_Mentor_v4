32aa0cdee706099ca3e4fd30c652dd9da3da1bc8
tigris8735 <den0208@bk.ru>
2025-10-08 22:22:10 +0300

    qqq

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Python version:"
-python3 --version
-echo "Pip version:"
-pip --version
-echo "Installing dependencies..."
-pip install -r requirements.txt
-echo "Starting bot..."
-python3 bot/webhook_server.py
